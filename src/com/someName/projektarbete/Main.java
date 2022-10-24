27661b5c07788a12c266ded8c78ae61cd65e31ef
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2022-10-24 13:49:42 +0200

    Todo and more

--- src/com/someName/projektarbete/Main.java
+++ src/com/someName/projektarbete/Main.java
@@ -1,15 +1,26 @@
 package com.someName.projektarbete;
 
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
         Deck deckTemplate = new Deck();
+        Random random = new Random();
 
         List<Card> deckOfCards = deckTemplate.generateDeck();
 
         System.out.println(deckOfCards);
+
+        System.out.println(deckOfCards.get(0));
+        System.out.println();
+        System.out.println(deckOfCards.remove(0));
+
+        // TODO - Being able to draw a card
+        // #1 - deckOfCards - 1 Card LESS
+        // #2 - Calculate Card Value
+        // #3 - Do While - Scanner + Switch cases
 
     }
 }
